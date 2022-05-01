7a571791353288caa918532e8d040f4b4cfb3055
mkharazi <mohsen.kharaziesfahani@cardtrend.com>
2022-05-01 16:06:23 +0800

    configuring routes, tying routes to action, placig the view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -9,6 +11,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,14 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "products", component: ProductListComponent},
+      {path: "products/:id", component : ProductDetailComponent},
+      {path: "welcome", component: WelcomeComponent},
+      {path: "", redirectTo: "welcome", pathMatch: "full"},
+      {path: "**", redirectTo: "welcome", pathMatch: "full"}
+    ])
   ],
   bootstrap: [AppComponent]
 })
